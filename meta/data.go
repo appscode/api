330438ee91e8c36a25ca2b8d64c83ec7b3ac64fc
shahriar <shahriar@appscode.com>
2016-08-17 13:07:30 +0600

    Modified "Notification" message for grouping
    Test Plan: API call
    
    Reviewers: tamal
    
    Reviewed By: tamal
    
    Differential Revision: https://appscode.appscode.io/D73

--- meta/data.go
+++ meta/data.go
@@ -83,7 +83,7 @@ func metaConfigYaml() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "meta/config.yaml", size: 883, mode: os.FileMode(436), modTime: time.Unix(1469499308, 0)}
+	info := bindataFileInfo{name: "meta/config.yaml", size: 883, mode: os.FileMode(436), modTime: time.Unix(1469511161, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
