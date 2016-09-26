76012300c1daf1a2d7dbdeeea3c37274f65f009a
sadlil <sadlil@appscode.com>
2016-09-26 16:30:57 +0600

    fix volume apis

--- glusterfs/v0.1/volume.pb.gw.go
+++ glusterfs/v0.1/volume.pb.gw.go
@@ -76,97 +76,6 @@ func request_Volumes_List_0(ctx context.Context, marshaler runtime.Marshaler, cl
 
 }
 
-func request_Volumes_Create_0(ctx context.Context, marshaler runtime.Marshaler, client VolumesClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq VolumeCreateRequest
-	var metadata runtime.ServerMetadata
-
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil {
-		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["kube_cluster"]
-	if !ok {
-		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "missing parameter %s", "kube_cluster")
-	}
-
-	protoReq.KubeCluster, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, err
-	}
-
-	msg, err := client.Create(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func request_Volumes_Delete_0(ctx context.Context, marshaler runtime.Marshaler, client VolumesClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq VolumeDeleteRequest
-	var metadata runtime.ServerMetadata
-
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["kube_cluster"]
-	if !ok {
-		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "missing parameter %s", "kube_cluster")
-	}
-
-	protoReq.KubeCluster, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, err
-	}
-
-	val, ok = pathParams["kube_namespace"]
-	if !ok {
-		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "missing parameter %s", "kube_namespace")
-	}
-
-	protoReq.KubeNamespace, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, err
-	}
-
-	val, ok = pathParams["glusterfs_cluster"]
-	if !ok {
-		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "missing parameter %s", "glusterfs_cluster")
-	}
-
-	protoReq.GlusterfsCluster, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, err
-	}
-
-	val, ok = pathParams["volume"]
-	if !ok {
-		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "missing parameter %s", "volume")
-	}
-
-	protoReq.Volume, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, err
-	}
-
-	msg, err := client.Delete(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
 // RegisterVolumesHandlerFromEndpoint is same as RegisterVolumesHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterVolumesHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -225,77 +134,13 @@ func RegisterVolumesHandler(ctx context.Context, mux *runtime.ServeMux, conn *gr
 
 	})
 
-	mux.Handle("PUT", pattern_Volumes_Create_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(ctx)
-		defer cancel()
-		if cn, ok := w.(http.CloseNotifier); ok {
-			go func(done <-chan struct{}, closed <-chan bool) {
-				select {
-				case <-done:
-				case <-closed:
-					cancel()
-				}
-			}(ctx.Done(), cn.CloseNotify())
-		}
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, req)
-		if err != nil {
-			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
-		}
-		resp, md, err := request_Volumes_Create_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_Volumes_Create_0(ctx, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	mux.Handle("DELETE", pattern_Volumes_Delete_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(ctx)
-		defer cancel()
-		if cn, ok := w.(http.CloseNotifier); ok {
-			go func(done <-chan struct{}, closed <-chan bool) {
-				select {
-				case <-done:
-				case <-closed:
-					cancel()
-				}
-			}(ctx.Done(), cn.CloseNotify())
-		}
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, req)
-		if err != nil {
-			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
-		}
-		resp, md, err := request_Volumes_Delete_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_Volumes_Delete_0(ctx, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	return nil
 }
 
 var (
 	pattern_Volumes_List_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5, 1, 0, 4, 1, 5, 6, 1, 0, 4, 1, 5, 7}, []string{"appscode", "api", "glusterfs", "v0.1", "volumes", "kube_cluster", "kube_namespace", "glusterfs_cluster"}, ""))
-
-	pattern_Volumes_Create_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"appscode", "api", "glusterfs", "v0.1", "volumes", "kube_cluster"}, ""))
-
-	pattern_Volumes_Delete_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5, 1, 0, 4, 1, 5, 6, 1, 0, 4, 1, 5, 7, 1, 0, 4, 1, 5, 8}, []string{"appscode", "api", "glusterfs", "v0.1", "volumes", "kube_cluster", "kube_namespace", "glusterfs_cluster", "volume"}, ""))
 )
 
 var (
 	forward_Volumes_List_0 = runtime.ForwardResponseMessage
-
-	forward_Volumes_Create_0 = runtime.ForwardResponseMessage
-
-	forward_Volumes_Delete_0 = runtime.ForwardResponseMessage
 )
--- glusterfs/v0.1/volume.schema.go
+++ glusterfs/v0.1/volume.schema.go
@@ -8,8 +8,6 @@ import (
 )
 
 var volumeListRequestSchema *gojsonschema.Schema
-var volumeCreateRequestSchema *gojsonschema.Schema
-var volumeDeleteRequestSchema *gojsonschema.Schema
 
 func init() {
 	var err error
@@ -31,61 +29,12 @@ func init() {
 	if err != nil {
 		glog.Fatal(err)
 	}
-	volumeCreateRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "properties": {
-    "glusterfs_cluster": {
-      "type": "string"
-    },
-    "kube_cluster": {
-      "type": "string"
-    },
-    "volume": {
-      "type": "string"
-    }
-  },
-  "type": "object"
-}`))
-	if err != nil {
-		glog.Fatal(err)
-	}
-	volumeDeleteRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "properties": {
-    "glusterfs_cluster": {
-      "type": "string"
-    },
-    "kube_cluster": {
-      "type": "string"
-    },
-    "kube_namespace": {
-      "type": "string"
-    },
-    "volume": {
-      "type": "string"
-    }
-  },
-  "type": "object"
-}`))
-	if err != nil {
-		glog.Fatal(err)
-	}
 }
 
 func (m *VolumeListRequest) IsValid() (*gojsonschema.Result, error) {
 	return volumeListRequestSchema.Validate(gojsonschema.NewGoLoader(m))
 }
 func (m *VolumeListRequest) IsRequest() {}
-
-func (m *VolumeCreateRequest) IsValid() (*gojsonschema.Result, error) {
-	return volumeCreateRequestSchema.Validate(gojsonschema.NewGoLoader(m))
-}
-func (m *VolumeCreateRequest) IsRequest() {}
-
-func (m *VolumeDeleteRequest) IsValid() (*gojsonschema.Result, error) {
-	return volumeDeleteRequestSchema.Validate(gojsonschema.NewGoLoader(m))
-}
-func (m *VolumeDeleteRequest) IsRequest() {}
 
 func (m *VolumeListResponse) SetStatus(s *dtypes.Status) {
 	m.Status = s
