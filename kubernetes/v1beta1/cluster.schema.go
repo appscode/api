29079f8f84e6832cb1686d0ec7d9c2015e6eb22b
tamal <tamal@appscode.com>
2016-10-17 11:25:47 -0700

    Add support for destroying load balancers and dynamic storage.

--- kubernetes/v1beta1/cluster.schema.go
+++ kubernetes/v1beta1/cluster.schema.go
@@ -127,6 +127,12 @@ func init() {
 	clusterDeleteRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
   "$schema": "http://json-schema.org/draft-04/schema#",
   "properties": {
+    "destroy_dynamic_volumes": {
+      "type": "boolean"
+    },
+    "destroy_lodabalancers": {
+      "type": "boolean"
+    },
     "force": {
       "type": "boolean"
     },
