f546c034bde7bddb74429301526fce2897b44db7
sadlil <sadlil@appscode.com>
2016-10-19 13:08:04 +0600

    removed disks names to disk size.
    Summary: this change is to fix the glusterfs from replication controller to petSet
    
    Test Plan: glusterfs
    
    Reviewers: tamal
    
    Differential Revision: https://appscode.appscode.io/D289

--- glusterfs/v1beta1/cluster.schema.go
+++ glusterfs/v1beta1/cluster.schema.go
@@ -76,11 +76,8 @@ func init() {
 	clusterCreateRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
   "$schema": "http://json-schema.org/draft-04/schema#",
   "properties": {
-    "disks": {
-      "items": {
-        "type": "string"
-      },
-      "type": "array"
+    "disk_size_gb": {
+      "type": "integer"
     },
     "kube_cluster": {
       "type": "string"
