49ee5be5d8f3291001eef31409b03713fe7a2b99
Tamal Saha <tamal@appscode.com>
2016-10-26 16:09:52 -0700

    Fix spelling mistake in INVALID_DATA.

--- pv/v1beta1/persistentvolumeclaim.pb.go
+++ pv/v1beta1/persistentvolumeclaim.pb.go
@@ -31,7 +31,7 @@ type PVCRegisterRequest struct {
 func (m *PVCRegisterRequest) Reset()                    { *m = PVCRegisterRequest{} }
 func (m *PVCRegisterRequest) String() string            { return proto.CompactTextString(m) }
 func (*PVCRegisterRequest) ProtoMessage()               {}
-func (*PVCRegisterRequest) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{0} }
+func (*PVCRegisterRequest) Descriptor() ([]byte, []int) { return fileDescriptor2, []int{0} }
 
 type PVCUnregisterRequest struct {
 	Cluster   string `protobuf:"bytes,1,opt,name=cluster" json:"cluster,omitempty"`
@@ -42,7 +42,7 @@ type PVCUnregisterRequest struct {
 func (m *PVCUnregisterRequest) Reset()                    { *m = PVCUnregisterRequest{} }
 func (m *PVCUnregisterRequest) String() string            { return proto.CompactTextString(m) }
 func (*PVCUnregisterRequest) ProtoMessage()               {}
-func (*PVCUnregisterRequest) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{1} }
+func (*PVCUnregisterRequest) Descriptor() ([]byte, []int) { return fileDescriptor2, []int{1} }
 
 type PVCDescribeRequest struct {
 	Cluster   string `protobuf:"bytes,1,opt,name=cluster" json:"cluster,omitempty"`
@@ -53,7 +53,7 @@ type PVCDescribeRequest struct {
 func (m *PVCDescribeRequest) Reset()                    { *m = PVCDescribeRequest{} }
 func (m *PVCDescribeRequest) String() string            { return proto.CompactTextString(m) }
 func (*PVCDescribeRequest) ProtoMessage()               {}
-func (*PVCDescribeRequest) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{2} }
+func (*PVCDescribeRequest) Descriptor() ([]byte, []int) { return fileDescriptor2, []int{2} }
 
 type PVCInfo struct {
 	Name        string   `protobuf:"bytes,1,opt,name=name" json:"name,omitempty"`
@@ -67,7 +67,7 @@ type PVCInfo struct {
 func (m *PVCInfo) Reset()                    { *m = PVCInfo{} }
 func (m *PVCInfo) String() string            { return proto.CompactTextString(m) }
 func (*PVCInfo) ProtoMessage()               {}
-func (*PVCInfo) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{3} }
+func (*PVCInfo) Descriptor() ([]byte, []int) { return fileDescriptor2, []int{3} }
 
 type PVCDescribeResponse struct {
 	Status *appscode_dtypes.Status `protobuf:"bytes,1,opt,name=status" json:"status,omitempty"`
@@ -77,7 +77,7 @@ type PVCDescribeResponse struct {
 func (m *PVCDescribeResponse) Reset()                    { *m = PVCDescribeResponse{} }
 func (m *PVCDescribeResponse) String() string            { return proto.CompactTextString(m) }
 func (*PVCDescribeResponse) ProtoMessage()               {}
-func (*PVCDescribeResponse) Descriptor() ([]byte, []int) { return fileDescriptor1, []int{4} }
+func (*PVCDescribeResponse) Descriptor() ([]byte, []int) { return fileDescriptor2, []int{4} }
 
 func (m *PVCDescribeResponse) GetStatus() *appscode_dtypes.Status {
 	if m != nil {
@@ -236,12 +236,12 @@ var _PersistentVolumeClaims_serviceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: fileDescriptor1,
+	Metadata: fileDescriptor2,
 }
 
-func init() { proto.RegisterFile("persistentvolumeclaim.proto", fileDescriptor1) }
+func init() { proto.RegisterFile("persistentvolumeclaim.proto", fileDescriptor2) }
 
-var fileDescriptor1 = []byte{
+var fileDescriptor2 = []byte{
 	// 539 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xbc, 0x54, 0xcf, 0x6e, 0xd3, 0x4e,
 	0x10, 0xd6, 0x26, 0x6d, 0xfe, 0x6c, 0x7e, 0xa7, 0xed, 0x4f, 0xad, 0xe5, 0x06, 0x08, 0xb9, 0x10,
