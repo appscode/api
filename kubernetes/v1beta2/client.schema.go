2b0906f9dad1a15761f3e7d49e7dbc71b4b53fe3
Tamal Saha <TamalSaha@users.noreply.github.com>
2017-03-23 04:55:50 -0700

    Remove regex for ancestor name. (#11)

--- kubernetes/v1beta2/client.schema.go
+++ kubernetes/v1beta2/client.schema.go
@@ -331,7 +331,6 @@ func init() {
       "properties": {
         "name": {
           "maxLength": 63,
-          "pattern": "^[a-z0-9](?:[a-z0-9\\-]{0,61}[a-z0-9])?$",
           "type": "string"
         },
         "type": {
