33eb674722b154635f17b5b81a4ace04742c5dc0
Tamal Saha <tamal@appscode.com>
2017-04-28 16:31:51 -0700

    Revise Login/Logout apis (#41)
     - Login() method can be used to sign in via token. Optionally a new token can issued on successful login.
     - Delete Token() method.
     - Logout() can only be called if already logged in.
     - Delete additional bindings

--- auth/v1beta1/authentication.schema.go
+++ auth/v1beta1/authentication.schema.go
@@ -7,25 +7,32 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 )
 
-var tokenRequestSchema *gojsonschema.Schema
-var logoutRequestSchema *gojsonschema.Schema
 var loginRequestSchema *gojsonschema.Schema
+var logoutRequestSchema *gojsonschema.Schema
 
 func init() {
 	var err error
-	tokenRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
+	loginRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
   "$schema": "http://json-schema.org/draft-04/schema#",
   "properties": {
+    "issue_token": {
+      "type": "boolean"
+    },
     "namespace": {
       "maxLength": 63,
       "pattern": "^[a-z0-9](?:[a-z0-9\\-]{0,61}[a-z0-9])?$",
       "type": "string"
     },
+    "password": {
+      "type": "string"
+    },
     "token": {
+      "type": "string"
+    },
+    "username": {
       "type": "string"
     }
   },
-  "title": "Next Id 4",
   "type": "object"
 }`))
 	if err != nil {
@@ -48,50 +55,19 @@ func init() {
 	if err != nil {
 		glog.Fatal(err)
 	}
-	loginRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "properties": {
-    "issue_token": {
-      "type": "boolean"
-    },
-    "namespace": {
-      "maxLength": 63,
-      "pattern": "^[a-z0-9](?:[a-z0-9\\-]{0,61}[a-z0-9])?$",
-      "type": "string"
-    },
-    "secret": {
-      "type": "string"
-    },
-    "username": {
-      "type": "string"
-    }
-  },
-  "type": "object"
-}`))
-	if err != nil {
-		glog.Fatal(err)
-	}
 }
-
-func (m *TokenRequest) IsValid() (*gojsonschema.Result, error) {
-	return tokenRequestSchema.Validate(gojsonschema.NewGoLoader(m))
-}
-func (m *TokenRequest) IsRequest() {}
-
-func (m *LogoutRequest) IsValid() (*gojsonschema.Result, error) {
-	return logoutRequestSchema.Validate(gojsonschema.NewGoLoader(m))
-}
-func (m *LogoutRequest) IsRequest() {}
 
 func (m *LoginRequest) IsValid() (*gojsonschema.Result, error) {
 	return loginRequestSchema.Validate(gojsonschema.NewGoLoader(m))
 }
 func (m *LoginRequest) IsRequest() {}
 
-func (m *CSRFTokenResponse) SetStatus(s *dtypes.Status) {
-	m.Status = s
+func (m *LogoutRequest) IsValid() (*gojsonschema.Result, error) {
+	return logoutRequestSchema.Validate(gojsonschema.NewGoLoader(m))
 }
-func (m *TokenResponse) SetStatus(s *dtypes.Status) {
+func (m *LogoutRequest) IsRequest() {}
+
+func (m *CSRFTokenResponse) SetStatus(s *dtypes.Status) {
 	m.Status = s
 }
 func (m *LogoutResponse) SetStatus(s *dtypes.Status) {
