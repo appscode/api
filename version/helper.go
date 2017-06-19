9562a3fc9884fb812b6152ea1865497dcfa0dbcd
Tamal Saha <tamal@appscode.com>
2017-06-19 13:58:48 -0700

    Print non-empty version metadata. (#66)

--- version/helper.go
+++ version/helper.go
@@ -6,18 +6,42 @@ func (m *Version) Print() {
 	if m.Name != "" {
 		fmt.Printf("Name = %v\n", m.Name)
 	}
-	fmt.Printf("Version = %v\n", m.Version)
-	fmt.Printf("VersionStrategy = %v\n", m.VersionStrategy)
-	fmt.Printf("Os = %v\n", m.Os)
-	fmt.Printf("Arch = %v\n", m.Arch)
+	if m.Version != "" {
+		fmt.Printf("Version = %v\n", m.Version)
+	}
+	if m.VersionStrategy != "" {
+		fmt.Printf("VersionStrategy = %v\n", m.VersionStrategy)
+	}
+	if m.Os != "" {
+		fmt.Printf("Os = %v\n", m.Os)
+	}
+	if m.Arch != "" {
+		fmt.Printf("Arch = %v\n", m.Arch)
+	}
 
-	fmt.Printf("CommitHash = %v\n", m.CommitHash)
-	fmt.Printf("GitBranch = %v\n", m.GitBranch)
-	fmt.Printf("GitTag = %v\n", m.GitTag)
-	fmt.Printf("CommitTimestamp = %v\n", m.CommitTimestamp)
+	if m.CommitHash != "" {
+		fmt.Printf("CommitHash = %v\n", m.CommitHash)
+	}
+	if m.GitBranch != "" {
+		fmt.Printf("GitBranch = %v\n", m.GitBranch)
+	}
+	if m.GitTag != "" {
+		fmt.Printf("GitTag = %v\n", m.GitTag)
+	}
+	if m.CommitTimestamp != "" {
+		fmt.Printf("CommitTimestamp = %v\n", m.CommitTimestamp)
+	}
 
-	fmt.Printf("BuildTimestamp = %v\n", m.BuildTimestamp)
-	fmt.Printf("BuildHost = %v\n", m.BuildHost)
-	fmt.Printf("BuildHostOs = %v\n", m.BuildHostOs)
-	fmt.Printf("BuildHostArch = %v\n", m.BuildHostArch)
+	if m.BuildTimestamp != "" {
+		fmt.Printf("BuildTimestamp = %v\n", m.BuildTimestamp)
+	}
+	if m.BuildHost != "" {
+		fmt.Printf("BuildHost = %v\n", m.BuildHost)
+	}
+	if m.BuildHostOs != "" {
+		fmt.Printf("BuildHostOs = %v\n", m.BuildHostOs)
+	}
+	if m.BuildHostArch != "" {
+		fmt.Printf("BuildHostArch = %v\n", m.BuildHostArch)
+	}
 }
